6d6f0bdd4088bd1fefc4aa9a6c2ee0d7f3902e84
TK Nguyen <tk@EverMind.local>
2017-06-10 19:47:58 +0700

    [DONE] Basic TipCal Function

--- TipCal/TipCal/ViewController.swift
+++ TipCal/TipCal/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLable: UILabel!
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var totalLable: UILabel!
+   
+    @IBOutlet weak var tipControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+   
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.18,0.2,0.25]
+        
+        let bill = Double(billTextField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLable.text = String(format: "$%.2f", tip)
+        totalLable.text = String(format: "$%.2f", total)
+    }
 
 }
 
