0d496a1d518c0f379b4ec6bc283ab18cb47b1896
TK Nguyen <tk@EverMind.local>
2017-06-10 21:42:51 +0700

    [DONE][Required]

--- TipCal/TipCal/ViewController.swift
+++ TipCal/TipCal/ViewController.swift
@@ -29,6 +29,10 @@ class ViewController: UIViewController {
         
     }
 
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
