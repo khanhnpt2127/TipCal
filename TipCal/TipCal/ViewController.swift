00dbca09075cb8dcc9ad456d9d391e9870c162c1
TK Nguyen <tk@EverMind.local>
2017-06-13 13:18:50 +0700

    Remember history

--- TipCal/TipCal/ViewController.swift
+++ TipCal/TipCal/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 
+extension Date {
+    var millisecondsSince1970:Int {
+        return Int((self.timeIntervalSince1970 * 1000.0).rounded())
+    }
+    
+    init(milliseconds:Int) {
+        self = Date(timeIntervalSince1970: TimeInterval(milliseconds / 1000))
+    }
+}
+
+
+
 
 class ViewController: UIViewController {
 
@@ -28,6 +40,53 @@ class ViewController: UIViewController {
         if UserDefaults.standard.object(forKey: "tipIndex") != nil {
             tipControl.selectedSegmentIndex = UserDefaults.standard.integer(forKey: "tipIndex")
         }
+        
+        if UserDefaults.standard.object(forKey: "lastMil") != nil{
+            let mil = UserDefaults.standard.integer(forKey: "lastMil")
+            let current = Date().millisecondsSince1970 - mil
+            if current < 600000 {
+                
+                
+                let lbill = (UserDefaults.standard.float(forKey: "lastVal"))
+                let lper = (UserDefaults.standard.integer(forKey: "lastPer"))
+                tipControl.selectedSegmentIndex = lper
+                billTextField.text = "\(lbill)"
+                switch lper {
+                case 0:
+                    let tip = Double(lbill) * 0.18
+                    let total = Double(lbill) + tip
+                    
+                    tipLable.text = String(format: "$%.1f", tip)
+                    totalLable.text = String(format: "$%.1f", total)
+                    
+                    break
+                case 1:
+                    let tip = Double(lbill) * 0.2
+                    let total = Double(lbill) + tip
+                    
+                    tipLable.text = String(format: "$%.1f", tip)
+                    totalLable.text = String(format: "$%.1f", total)
+                    
+                    break;
+                case 2:
+                    let tip = Double(lbill) * 0.25
+                    let total = Double(lbill) + tip
+                    
+                    tipLable.text = String(format: "$%.1f", tip)
+                    totalLable.text = String(format: "$%.1f", total)
+                    
+                    break;
+                default:
+                    break
+                }
+                
+                
+                
+                
+                
+            }
+        }
+        
         
         
     }
@@ -64,6 +123,12 @@ class ViewController: UIViewController {
         tipLable.text = String(format: "$%.1f", tip)
         totalLable.text = String(format: "$%.1f", total)
         
+        let defaults = UserDefaults.standard
+        
+        defaults.set(Date().millisecondsSince1970, forKey: "lastMil")
+        defaults.set(bill, forKey: "lastVal")
+        defaults.set(tipControl.selectedSegmentIndex, forKey: "lastPer")
+        defaults.synchronize()
         
     }
     
